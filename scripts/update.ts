cfe17c60519c2985b28df213334cf23e3a3f67b8
Piotr Brzozowski <brzozowski.s.piotr@gmail.com>
2020-10-24 16:06:08 +0200

    Revert "Add docs refresh when notes change"
    This reverts commit e338e028

--- scripts/update.ts
+++ scripts/update.ts
@@ -8,7 +8,6 @@ import { versionsPath, tmpDirectory, FileTypes } from './lib/constants.ts'
 type TagsType = {
   stable: [string, string]
   beta: [string, string]
-  notes: string;
 }
 
 type UpdateVersionType = {
@@ -27,12 +26,6 @@ type ResultType = {
 }
 
 const main = async (force: boolean = false) => {
-  //Get repo name from args, fallback to hardcoded value
-  const repoName = Deno.args[0] || "stirante/docs";
-  //Get last commit date for notes folder from GH API
-  const lastNotes = await fetch("https://api.github.com/repos/" + repoName + "/commits?path=notes")
-      .then(value => value.text())
-      .then(value => JSON.parse(value)[0].commit.committer.date);
   const { minor: betaMinor, major: betaMajor, path: betaPath } = await unzipDocumentationFiles(FileTypes.Beta)
   const { minor: retailMinor, major: retailMajor, path: retailPath } = await unzipDocumentationFiles(FileTypes.Retail)
 
@@ -41,7 +34,6 @@ const main = async (force: boolean = false) => {
 
   let newBeta = false
   let newRetail = false
-  let newNotes = false
 
   if (versions.beta[1] !== betaMinor) {
     newBeta = true
@@ -51,17 +43,13 @@ const main = async (force: boolean = false) => {
     newRetail = true
     versions.stable = [ retailMajor, retailMinor ]
   }
-  if (versions.notes !== lastNotes) {
-    newNotes = true
-    versions.notes = lastNotes
-  }
 
-  if (newBeta || newRetail || force || newNotes) {
+  if (newBeta || newRetail || force) {
     // update the file containing the mapping
     writeFileStrSync(versionsPath, JSON.stringify(versions, null, 2))
 
-    if (newBeta || force || newNotes) copyDocumentationFiles(FileTypes.Beta, betaPath, versions.beta)
-    if (newRetail || force || newNotes) copyDocumentationFiles(FileTypes.Retail, retailPath, versions.stable)
+    if (newBeta || force) copyDocumentationFiles(FileTypes.Beta, betaPath, versions.beta)
+    if (newRetail || force) copyDocumentationFiles(FileTypes.Retail, retailPath, versions.stable)
   }
 
   let result: ResultType = {
@@ -75,20 +63,21 @@ const main = async (force: boolean = false) => {
 
   // talk to github actions
   if (result.update) {
-    if (newBeta || newNotes) {
+    if (newBeta) {
       result.update.beta = {
         updated: true,
         name: betaMinor,
         path: [betaMajor, betaMinor].join('/')
       }
     }
-    if (newRetail || newNotes) {
+    if (newRetail) {
       result.update.stable = {
         updated: true,
         name: retailMinor,
         path: [retailMajor, retailMinor].join('/')
       }
     }
+    
   }
 
   Deno.removeSync(resolve(tmpDirectory), { recursive: true })
@@ -101,8 +90,7 @@ const main = async (force: boolean = false) => {
   try {
     output = await main()
   } catch (e) {
-    //Use toString, because some errors are blank
-    output = { error: e.toString() }
+    output = { error: e }
   }
   console.log(JSON.stringify(output))
 })()
