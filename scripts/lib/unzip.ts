51e7d8a0467b8b7f3d65f0c79afb32913ef5e042
Piotr Brzozowski <brzozowski.s.piotr@gmail.com>
2020-10-24 18:12:17 +0200

    " -> '

--- scripts/lib/unzip.ts
+++ scripts/lib/unzip.ts
@@ -1,25 +1,25 @@
 import {
   readFileStrSync,
   writeFileStrSync,
-} from "https://deno.land/std@0.61.0/fs/mod.ts";
-import { resolve } from "https://deno.land/std@0.61.0/path/mod.ts";
+} from 'https://deno.land/std@0.61.0/fs/mod.ts';
+import { resolve } from 'https://deno.land/std@0.61.0/path/mod.ts';
 
-import { unzip } from "https://unpkg.com/unzipit@1.3.1/dist/unzipit.module.js";
-import { Buffer } from "https://deno.land/std@0.74.0/node/buffer.ts";
+import { unzip } from 'https://unpkg.com/unzipit@1.3.1/dist/unzipit.module.js';
+import { Buffer } from 'https://deno.land/std@0.74.0/node/buffer.ts';
 
 import {
   mkdirSync,
   writeFileSync,
-} from "https://deno.land/std@0.74.0/node/fs.ts";
+} from 'https://deno.land/std@0.74.0/node/fs.ts';
 
-import { FileTypes, tmpDirectory, urls } from "./constants.ts";
-import { formatSchemas } from "./clean.ts";
-import { addNotes } from "./notes.ts";
+import { FileTypes, tmpDirectory, urls } from './constants.ts';
+import { formatSchemas } from './clean.ts';
+import { addNotes } from './notes.ts';
 
 export const unzipDocumentationFiles = async (type: FileTypes) => {
   const url = urls[type].b;
 
-  const docsPath = resolve(tmpDirectory + type) + "/";
+  const docsPath = resolve(tmpDirectory + type) + '/';
   try {
     mkdirSync(docsPath, { recursive: true });
   } catch (e) {
@@ -28,9 +28,9 @@ export const unzipDocumentationFiles = async (type: FileTypes) => {
   const { entries } = await unzip(url);
 
   for (const [name, entry] of Object.entries(entries)) {
-    if (name.startsWith("documentation/") && !(entry as any).isDirectory) {
+    if (name.startsWith('documentation/') && !(entry as any).isDirectory) {
       const localLocation = resolve(
-        docsPath + name.replace("documentation/", ""),
+        docsPath + name.replace('documentation/', ''),
       );
       const buffer = await (entry as any).arrayBuffer();
       let unit8arr = addNotes(new Buffer(buffer), name);
@@ -38,25 +38,25 @@ export const unzipDocumentationFiles = async (type: FileTypes) => {
     }
   }
 
-  let minorVersion: string = "";
-  let majorVersion: string = "";
+  let minorVersion: string = '';
+  let majorVersion: string = '';
 
   // read from the unused index file the version
-  const indexFile = readFileStrSync(resolve(docsPath + "/Index.html"));
+  const indexFile = readFileStrSync(resolve(docsPath + '/Index.html'));
   let versionString = indexFile.match(/Version: (\d+\.\d+\.\d+\.\d+)/);
   if (versionString) {
     if (versionString.length > 1) {
       minorVersion = versionString[1];
 
-      const versionParts = minorVersion.split(".");
+      const versionParts = minorVersion.split('.');
       majorVersion = `${versionParts[0]}.${versionParts[1]}.0.0`;
     }
   }
 
   // delete the index file
-  Deno.removeSync(resolve(docsPath + "/Index.html"));
+  Deno.removeSync(resolve(docsPath + '/Index.html'));
 
-  const schemasFilePath = resolve(docsPath + "/Schemas.html");
+  const schemasFilePath = resolve(docsPath + '/Schemas.html');
   const schemasFile = readFileStrSync(schemasFilePath);
   const formattedSchemas = formatSchemas(schemasFile);
   writeFileStrSync(schemasFilePath, formattedSchemas);
